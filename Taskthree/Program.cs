93b50c62749b941b63ad64a5082f20ec61062453
Saggitarius69 <obitothapa2061@gmail.com>
2025-11-10 11:58:59 +0545

    Taskthree Completed

---
+++ Taskthree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        string input = Console.ReadLine();
+
+        if (int.TryParse(input, out int age))
+            {
+            
+                if (age < 13)
+                {
+                    Console.WriteLine("Child.");
+                }
+                else if (age < 20)
+                {
+                    Console.WriteLine("Teenager.");
+                }
+                else if (age >= 20)
+                {
+                    Console.WriteLine("Adult.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter a valid number.");
+            }
+
+    }
+}
